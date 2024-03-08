051a15a2f070989721efd35550118430ed47eb5f
CARLOSMARES <46010587+CARLOSMARES@users.noreply.github.com>
2024-03-08 00:45:35 -0600

    feat: :memo: Persistencia de Datos
    Se configura la api para usar turso como base de datos, y guardar los mensajes y mostrarlos de forma permanente, se programa la opcion para mostrar desde el ultimo mensaje leido

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ import express from 'express';
 import logger from 'morgan';
 import { Server } from 'socket.io';
 import { createServer } from 'node:http'
+import dotenv from 'dotenv';
+import { createClient } from '@libsql/client';
+
+dotenv.config();
 
 const port = process.env.PORT ?? 3000;
 
@@ -10,18 +14,61 @@ const app = express();
 
 const server = createServer(app);
 
-const io = new Server(server);
+const db = createClient({
+    url: `libsql://driving-blue-marvel-carlosmares.turso.io`,
+    authToken: process.env.DB_TOKEN,
+})
 
-io.on("connection", (socket)=>{
+await db.execute(`
+    CREATE TABLE IF NOT EXISTS messages(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        content TEXT
+    )
+`)
+
+const io = new Server(server,{
+    connectionStateRecovery: {
+    }
+});
+
+io.on("connection", async (socket)=>{
     console.log('a user has connected');
 
     socket.on('disconnect', ()=>{
         console.log('a user has desconnecteds')
     })
 
-    socket.on('chat message', (msg)=>{
-        io.emit('chat message', msg);
+    socket.on('chat message', async (msg, serverOffset)=>{
+        let result
+        try{
+            result = await db.execute({
+                sql: `INSERT INTO messages (content) VALUES (:content)`,
+                args: { content: msg }
+            })
+        }
+        catch (e){
+            console.error(e);
+            return;
+        }
+        io.emit('chat message', msg, result.lastInsertRowid.toString());
     })
+    if(!socket.recovered){
+        try{
+            const result = await db.execute({
+                sql: `SELECT id, content FROM messages WHERE id > ?`,
+                args: [
+                    socket.handshake.auth.serverOffset ?? 0 
+                ]
+            })
+            result.rows.forEach(row =>{
+                socket.emit('chat message', row.content, row.id.toString());
+            })
+        }
+        catch(e){
+            console.error(e);
+            return;
+        }
+    }
 })
 
 app.use(logger('dev'))
@@ -36,5 +83,3 @@ server.listen(port, (err)=>{
     }
     console.log(`Server running on port ${port}`);
 })
-
-module.exports = server;
