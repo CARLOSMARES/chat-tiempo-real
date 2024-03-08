b095d9f1012110b68b9d5f57a96fba0782dccdd7
CARLOSMARES <46010587+CARLOSMARES@users.noreply.github.com>
2024-03-07 20:36:10 -0600

    build: :white_check_mark: Se crea la interfaz y la funcionalidad basica del chat con Socket.io y nodejs
    Se crea la interfaz con html, css y javascript, se crea la api que permite el envio y recepcion de mensajes con nodejs, express y socket.io

---
+++ server/index.js
@@ -0,0 +1,38 @@
+'use strict'
+import express from 'express';
+import logger from 'morgan';
+import { Server } from 'socket.io';
+import { createServer } from 'node:http'
+
+const port = process.env.PORT ?? 3000;
+
+const app = express();
+
+const server = createServer(app);
+
+const io = new Server(server);
+
+io.on("connection", (socket)=>{
+    console.log('a user has connected');
+
+    socket.on('disconnect', ()=>{
+        console.log('a user has desconnecteds')
+    })
+
+    socket.on('chat message', (msg)=>{
+        io.emit('chat message', msg);
+    })
+})
+
+app.use(logger('dev'))
+
+app.get("/", (req, res)=>{
+    res.sendFile(process.cwd() + `/client/index.html`)
+})
+
+server.listen(port, (err)=>{
+    if(err){
+        console.error(err);
+    }
+    console.log(`Server running on port ${port}`);
+})
