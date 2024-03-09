f47426eda94ebd2a31174f840c054a574d016a84
CARLOSMARES <46010587+CARLOSMARES@users.noreply.github.com>
2024-03-08 18:57:48 -0600

    process.env.DB_TOKEN

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const server = createServer(app);
 
 const db = createClient({
     url: `libsql://driving-blue-marvel-carlosmares.turso.io`,
-    authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MDk4NzgwNjAsImlkIjoiZDE5YzAwNWMtN2ZkMS00ZTFiLWE1MmMtODAxOTVlZTVhMWU4In0.g3R8dMqzTn0LI6ASxGI1VM_bb1YqLVsoq7yHOAwGDG13yT7yBhayWPpa0t5DPutuDe5uQT-L8EIZwWnfhhbHBg"//process.env.DB_TOKEN,
+    authToken: process.env.DB_TOKEN,
 })
 
 await db.execute(`
